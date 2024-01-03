4e9798933d5340b1dce0bd3ba1114175e485766e
UnknownCmds <emperol171617@gmail.com>
2024-01-03 16:21:37 +0100

    xd

---
+++ src/test/java/com/prototype/ChessT/ChessTournamentApplicationTest.java
@@ -0,0 +1,34 @@
+package com.prototype.ChessT;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChessTournamentApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void fideUpdate() {
+        //fail();
+    }
+
+    @Test
+    void homepage() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void kValue() {
+    }
+
+    @Test
+    void stringToDate() {
+    }
+}
