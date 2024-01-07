e96c5c40ce13478f1b5422cee87778b3965e42df
UnknownCmds <emperol171617@gmail.com>
2024-01-07 18:27:25 +0100

    new files

---
+++ src/test/java/com/prototype/ChessT/TournamentTest.java
@@ -0,0 +1,60 @@
+package com.prototype.ChessT;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TournamentTest {
+
+    @Test
+    void firstRoundPairings() {
+    }
+
+    @Test
+    void generateRoundPairings() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void join() {
+    }
+
+    @Test
+    void tournamentInfo() {
+    }
+
+    @Test
+    void getMatch() {
+    }
+
+    @Test
+    void getRound() {
+    }
+
+    @Test
+    void playerInfo() {
+    }
+
+    @Test
+    void results() {
+    }
+
+    @Test
+    void updateMatch() {
+    }
+
+    @Test
+    void startTournament() {
+    }
+
+    @Test
+    void endTournament() {
+    }
+
+    @Test
+    void changeRoundsNo() {
+    }
+}
---
+++ src/test/java/com/prototype/ChessT/UserTest.java
@@ -0,0 +1,60 @@
+package com.prototype.ChessT;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void user() {
+    }
+
+    @Test
+    void account() {
+    }
+
+    @Test
+    void validate() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void myTournaments() {
+    }
+
+    @Test
+    void randomString32Char() {
+    }
+
+    @Test
+    void addAuthCookie() {
+    }
+
+    @Test
+    void checkCookie() {
+    }
+
+    @Test
+    void checkFalseCookie() {
+    }
+
+    @Test
+    void hashPassword() {
+    }
+
+    @Test
+    void testValidate() {
+    }
+}
